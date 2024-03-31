8ddd08341696e6ce0bca4034d28c96f8c0837870
Haru-Kazumoto <jiaathaya@gmail.com>
2024-03-31 16:52:07 +0700

    fix: fix where condition for get all redeem code

--- src/modules/redeem_code/repositories/redeem_code.repository.ts
+++ src/modules/redeem_code/repositories/redeem_code.repository.ts
@@ -3,6 +3,7 @@ import { RedeemCode } from "../entities/redeem_code.entity";
 import { Injectable } from "@nestjs/common";
 import { PageDto, PageOptionsDto } from "src/utils/pagination.utils";
 import { pagination } from "src/utils/modules_utils/pagination.utils";
+import { StatusCode } from "src/enums/status_code.enum";
 
 @Injectable()
 export class RedeemCodeRepository extends Repository<RedeemCode> {
@@ -16,12 +17,14 @@ export class RedeemCodeRepository extends Repository<RedeemCode> {
             .getOne();
     }
     
-    async findManyCode(filterStatus: "VALID" | "NOT VALID",pageOptionsDto: PageOptionsDto): Promise<PageDto<RedeemCode>>{
+    async findManyCode(filterStatus: StatusCode,pageOptionsDto: PageOptionsDto): Promise<PageDto<RedeemCode>>{
         const queryAlias: string = "redeem_code";
 
         const whereCondition = (qb: SelectQueryBuilder<RedeemCode>) => {
-            if(filterStatus === "VALID") qb.where(`${queryAlias}.is_valid = true`);
-            else qb.andWhere(`${queryAlias}.is_valid = false`);
+            if(filterStatus === StatusCode.VALID) {
+                qb.andWhere(`${queryAlias}.is_valid = true`)
+            };
+            // else qb.andWhere(`${queryAlias}.is_valid = false`);
         };
 
         return pagination<RedeemCode>(this, pageOptionsDto, queryAlias, whereCondition);
